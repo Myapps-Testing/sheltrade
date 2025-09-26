5cabafb99d250a70b6d578046d688a9e3923671c
Myapps-Testing <myappsa9@gmail.com>
2025-09-27 00:35:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "::", // IPv6-compatible, you could also use '0.0.0.0'
     port: 8080,
   },
   preview: {
-    port: 4173,
     host: true,
+    port: 4173,
     allowedHosts: ['sheltrade-ig8i.onrender.com'],
-}
-
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {

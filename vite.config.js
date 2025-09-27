db21b155d16ea6812a2c9c7e2f76ef78af106f21
Myapps-Testing <myappsa9@gmail.com>
2025-09-27 16:00:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    allowedHosts: ["sheltrade-d2pl.onrender.com"],
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ["sheltrade-d2pl.onrender.com"],
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));

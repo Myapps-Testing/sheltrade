c56cf5f30651f8fc29710cdb5c7224cd56648cf6
Myapps-Testing <myappsa9@gmail.com>
2025-09-27 01:15:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   preview: {
     host: true,
     port: 4173,
-    allowedHosts: ['sheltrade-ig8i.onrender.com'],
+    allowedHosts: 'all'
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {

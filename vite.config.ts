52a295832989f0f3fd053578068fe25ceef8bfd3
Myapps-Testing <myappsa9@gmail.com>
2025-09-27 14:22:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Render expects this folder
+  },
   server: {
-    host: true,
-    port: 4173,
-    allowedHosts: ['sheltrade-ig8i.onrender.com', 'https://sheltrade-ig8i.onrender.com']
+    port: 3000, // Render will map this automatically
+    strictPort: true,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
